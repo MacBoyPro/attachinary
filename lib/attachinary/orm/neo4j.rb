363024539b25ae5bd648fafe51599893aaf98898
Lee Lundrigan <Lee.Lundrigan@gmail.com>
2015-12-29 13:39:25 -0800

    Create neo4j.rb

---
+++ lib/attachinary/orm/neo4j.rb
@@ -0,0 +1,6 @@
+require_relative 'file_mixin'
+require_relative 'base_extension'
+require_relative 'neo4j/extension'
+require_relative 'neo4j/file'
+
+Neo4j::ActiveNode::ClassMethods.send :include, Attachinary::Extension
