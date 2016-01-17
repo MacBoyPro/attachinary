02f72be07a87bbec32228ca79d866756a09d1d6e
MacBoyPro <lee.lundrigan@gmail.com>
2016-01-17 10:11:13 -0800

    try anything

--- lib/attachinary/orm/neo4j/file.rb
+++ lib/attachinary/orm/neo4j/file.rb
@@ -1,9 +1,9 @@
 module Attachinary
   class File
-    include ::Neo4j::ActiveNode
-    include ::Neo4j::Timestamps
+    include Neo4j::ActiveNode
+    include Neo4j::Timestamps
     include FileMixin
     
-    belongs_to :attachinariable, polymorphic: true
+    has_one :out, :attachinariable, type: 'attachinariable', model_class: false
   end
 end
